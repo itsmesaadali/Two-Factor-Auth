b5d80d7b0e7b025151e1bf19168ab8c2b24c7ab1
itsmesaadali <itmesaad@gmail.com>
2025-08-18 08:25:45 +0500

    All good in backend

--- backend/src/config/app.config.ts
+++ backend/src/config/app.config.ts
@@ -3,7 +3,7 @@ import { getEnv } from "../common/utils/get-env";
 const appConfig = () => ({
     NODE_ENV: getEnv('NODE_ENV', 'development'),
     APP_ORIGIN: getEnv('APP_ORIGIN', 'localhost'),
-    PORT: getEnv('PORT', '3000'),
+    PORT: getEnv('PORT', '5000'),
     BASE_PATH: getEnv('BASE_PATH', '/api/v1'),
     MONGO_URI: getEnv('MONGO_URI'),
 
